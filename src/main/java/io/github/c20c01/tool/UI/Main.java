a03dca79a2c68d1bffc0b70e34b1a2a42429da9d
C20C01 <cc_20_01@163.com>
2022-02-26 14:35:37 +0800

    .

--- src/main/java/io/github/c20c01/tool/UI/Main.java
+++ src/main/java/io/github/c20c01/tool/UI/Main.java
@@ -1,12 +1,7 @@
 package io.github.c20c01.tool.UI;
 
-import io.github.c20c01.tool.MessageTool;
-import io.github.c20c01.tool.TimeTool;
-
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class Main extends JFrame {
 
@@ -25,6 +20,6 @@ public class Main extends JFrame {
     }
 
     public static void main(String[] args) {
-        System.out.println(MessageTool.readString("{\"translate\":\"chat.type.text\",\"with\":[{\"insertion\":\"8899\",\"clickEvent\":{\"action\":\"suggest_command\",\"value\":\"/tell 8899 \"},\"hoverEvent\":{\"action\":\"show_entity\",\"contents\":{\"type\":\"minecraft:player\",\"id\":\"94b58eab-d528-36de-b7eb-039e01271aa7\",\"name\":{\"text\":\"8899\"}}},\"text\":\"8899\"},\"66666666666666666\"]}"));
+
     }
 }
